51e97976f394dd63a900c1717184f56147a884b6
merefield <merefield@gmail.com>
2024-11-17 22:29:17 +0000

    globalise React object

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+window.React = require('react')
+
 const root = ReactDOM.createRoot(document.getElementById('my-widget'));
 root.render(
   <React.StrictMode>
