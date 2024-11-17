aba693ae789a20f609dfe92840a4a21470a7ba4d
merefield <merefield@gmail.com>
2024-11-17 22:31:59 +0000

    remove redundant globalise

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-window.React = require('react')
-
 const root = ReactDOM.createRoot(document.getElementById('my-widget'));
 root.render(
   <React.StrictMode>
